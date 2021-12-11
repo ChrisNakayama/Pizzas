c08b0484e49a5d8eb563cfb751493768e68eb56c
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-12-10 16:20:26 -0800

    HTML and JS WORK Mapping out project.

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,26 @@ Pizza.prototype.addMeat = function(meat) {
 }  
 Pizza.prototype.addVeg = function(veggie) {
     this.vegToppings.push(veggie);
-} 
+}   
+  Pizza.prototype.refreshCost = function() {
+    var cost = 0;
+    if (this.pizzaSize === "small") {
+      cost = 5;
+    } else if (this.pizzaSize === "medium") {
+      cost = 10;
+    } else if (this.pizzaSize === "large") {
+      cost = 15;
+    } else if (this.pizzaSize === "extra large") {
+      cost = 20;
+    } 
+    this.meatToppings.forEach(function() {
+      cost += 2;
+    });  
+    this.vegToppings.forEach(function() {
+      cost += 1;
+    });
+    if (this.cheese === "extra") {
+      cost += 1;
+    } 
+    this.cost = cost;   
+}      
